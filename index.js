f6a088feade154430228223a732c6902ce7853c6
Michael Lombard <miklombard@yahoo.com>
2023-05-07 17:06:03 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(anArrayOfNames, event) {
+    let messages = [];
+    for (let i = 0; i < anArrayOfNames.length; i++) {
+        //console.log(`Thank you, ${anArrayOfNames[i]}, for the wonderful ${event} gift!`);
+        messages.push(`Thank you, ${anArrayOfNames[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+function countDown(positiveNumber){
+    let i = 10;
+    while (i >= 0) {
+      console.log(i--);
+    }
+
+}
